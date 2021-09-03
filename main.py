05ce5273131b07b266c1dbc5cd19bd6a54b96171
Ahmet Efe AKYAZI <46342237+efe3535@users.noreply.github.com>
2021-09-03 10:59:58 +0300

    Added words, excuses will be funnier.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from random import choice
 
 excuses = [
-    [ "I was ", "My neighbor was ", "We were ", "My mom was " ],
-    [ "washing my ", "eating a ", "damaging a ", "praying to a " ],
-    [ "car.", "cat.", "couch.", "door." ]
+    [ "I was ", "My neighbor was ", "We were ", "My mom was ", "My sister was ", "My brother was ", "My girlfriend was ", "My boyfriend was", "My uncle was" ],
+    [ "washing my ", "eating a ", "damaging a ", "praying to a ", "riding a ", "making a ", "going through a portal with a ", "invading America for a ", "forgot everything because of a " ],
+    [ "car.", "cat.", "couch.", "door.", "television.", "computer case.", "carpet", "grenade.", "soldier.", "lettuce." ]
 ]
 
 # A funny list, you can create a pull request to add something :]
