aee206f42422438658bada40caebaea297f1ae45
Akif9748 <70021050+Akif9748@users.noreply.github.com>
2021-09-12 23:10:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from random import choice
 
 excuses = [
     [ "I was ", "My neighbor was ", "We were ", "My mom was ", "My sister was ", "My brother was ", "My girlfriend was ", "My boyfriend was ", "My uncle was " ],
-    [ "washing my ", "eating a ", "damaging a ", "praying to a ", "riding a ", "making a ", "going through a portal with a ", "invading America for a ", "forgot everything because of a " ],
-    [ "car.", "cat.", "couch.", "door.", "television.", "computer case.", "carpet", "grenade.", "soldier.", "lettuce." ]
+    [ "washing my ", "eating a ", "damaging a ", "praying to a ", "riding a ", "making a ", "going through a portal with a ", "invading America for a ", "forgot everything because of a ", "giving birth to " ],
+    [ "car.", "cat.", "couch.", "door.", "television.", "computer case.", "carpet", "grenade.", "soldier.", "lettuce.", "my cousin." ]
 ]
 
 # A funny list, you can create a pull request to add something :]
